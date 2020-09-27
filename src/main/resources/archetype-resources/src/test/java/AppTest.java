36a966c52c39a5d96d7a21edad70b0dcc1e1d819
Andrei Lorin <andreilorin@gmail.com>
2020-09-27 22:44:28 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertThat( true );
+    }
+}
